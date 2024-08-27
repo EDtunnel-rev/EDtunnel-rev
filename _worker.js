b7373a1e43ba39140906108e5d48c26635a18aea
putangina17 <ariskeesa@gmail.com>
2024-08-27 15:26:00 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,9 +3,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'f78e3259-7f9d-4c11-bc5f-9c133b1a6863';
 
-const พร็อกซีไอพีs = ['51.79.254.182', 'http://testt.leov.asia/linuxdo/ip.php', '194.58.56.87', '129.150.37.203', '18.141.204.88', '202.10.42.30', '52.74.101.26', '8.219.98.13'];
+const พร็อกซีไอพีs = ['194.58.56.111'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
