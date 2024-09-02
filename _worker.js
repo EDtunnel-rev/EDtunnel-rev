2f34e960b99bf5190b90771bd1e1e17f3082d7bc
erap0 <raul.palmawanto@mhs.unsoed.ac.id>
2024-09-03 03:09:57 +0700

    Update _worker.js
    server aus

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-const พร็อกซีไอพีs = ['138.2.79.156'];
+const พร็อกซีไอพีs = ['192.9.177.204'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
