3768908355cef4b98976fa064b8993512ca449a0
Hermananza <101302614+Hermananza@users.noreply.github.com>
2024-09-24 00:37:29 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -286,7 +286,7 @@ function homePageHTML() {
     <div class="sidebar">
         <div>
             <h2>Navigation</h2>
-            <a href="wa.me/+6285162223311" target="_blank"><i class="fas fa-info-circle"></i> Order Akun </a>
+            <a href="https://wa.me/+6285162223311" target="_blank"><i class="fas fa-info-circle"></i> Order Akun </a>
             <a href="https://t.me/edtunrev" target="_blank"><i class="fab fa-telegram"></i> Telegram Group</a>
         </div>
         <div>
