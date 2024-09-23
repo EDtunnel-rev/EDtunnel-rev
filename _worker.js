a89907825dbdc046d05cd9fc29cb6dba2e757726
Hermananza <101302614+Hermananza@users.noreply.github.com>
2024-09-23 12:11:45 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'cec361ec-074b-4bfc-8073-1fc6a17722c9';
 
-const พร็อกซีไอพีs = ['212.146.246.165'];
+const พร็อกซีไอพีs = ['ip.hc-store.my.id'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
